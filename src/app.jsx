47dd8f16dfe02163f7d17e9a2c1bba3b5648315c
lesia <chlesya@yandex.ru>
2024-11-17 19:44:01 +0300

    3 comps

--- src/app.jsx
+++ src/app.jsx
@@ -1,45 +1,4 @@
- import { restaurants } from "./mock";
-import { Reviews } from "./compons/reviews/reviews";
- import { Menu } from "./compons/menu/menu";
-//import { RestaurantsPage } from "./compons/restaurants-page/restaurants-page"
-export const App =({title})=>{
-    return (  <div>
-    <h1>Restaurants {title}</h1>
-{
- restaurants.map(({name, id, menu, reviews}) =>(
-  <div key={id}>
-      <h2>{name}</h2>
-      <Menu menu={menu}/>
-     
-      <Reviews reviews={reviews}/>
-      
-    
-      </div>
-)
-
-)
-}
-
-    </div>
-    )
-    //  <RestaurantsPage></RestaurantsPage>
-     //()   
-//         <div>
-//         <h1>Restaurants {title}</h1>
-// {
-//      restaurants.map(({name, id, menu, reviews}) =>(
-   
-    //   <div key={id}>
-    //       <h2>{name}</h2>
-    //       <Menu menu={menu}/>
- 
-    //       <Reviews reviews={reviews}/>
-    //       </div>
-    //)
-    
-   // )
-   // }
-
-      //  </div>
-    //)
+import { RestaurantsPage } from "./compons/restaurants-page/restaurants-page"
+export const App =()=>{
+    return <RestaurantsPage></RestaurantsPage>
 }
--- src/compons/restaurants-page/restaurants-page.jsx
+++ src/compons/restaurants-page/restaurants-page.jsx
@@ -1,25 +1,19 @@
-// import { restaurants } from "../../mock";
-// import { Reviews } from "./compons/reviews/reviews";
-// import { Menu } from "./compons/menu/menu";
-// export const RestaurantsPage=({props})=>{
-//     return(
-//         <div>
-//         <h1>Restaurants {title}</h1>
-// {
-//      restaurants.map(({name, id, menu, reviews}) =>(
-//       <div key={id}>
-//           <h2>{name}</h2>
-//           <Menu menu={menu}/>
-         
-//           <Reviews reviews={reviews}/>
-          
-        
-//           </div>
-//     )
-    
-//     )
-//     }
+import { restaurants } from "../../mock";
+import {Reviews} from "../../compons/reviews/reviews";
+import { Menu} from "../../compons/menu/menu";
 
-//         </div>
-//     )
-// }
+export const RestaurantsPage=({props})=>{
+    return(
+        <div>
+        <h1>Restaurants 6</h1>
+        { restaurants.map(({name, id, menu, reviews}) =>(
+          <div key={id}>
+          <h2>{name}</h2>
+          <Menu menu={menu}/>
+          <Reviews reviews={reviews}/>
+          </div>
+    ) ) }
+
+        </div>
+    );
+};
