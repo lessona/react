91d67386df3f94a70421cf836e9d8e5ec760d853
lesia <chlesya@yandex.ru>
2024-11-26 18:23:24 +0300

    context

--- src/compons/layout/layout.jsx
+++ src/compons/layout/layout.jsx
@@ -2,14 +2,28 @@ import { Footer } from "../footer/footer";
 import { Header } from "../header/header";
 import { ProgressBar } from "../progress-bar/progress-bar";
 import styles from "./layout.module.css";
+import { ThemeContextProvider } from "../theme-context/theme-context.jsx";
+import { AuthContext } from "../../autocontext/autocontext.jsx";
+
 export const Layout = ({ children, sidebar }) => {
   return (
-    <div>
-      <ProgressBar />
-      <Header />
-      <div className={styles.layout}>{sidebar}</div>
-      {children}
-      <Footer />
-    </div>
+    <AuthContext.Provider value={true}>
+      <ThemeContextProvider>
+        <div>
+          <ProgressBar viewVariant="colored" />
+          <Header />
+          {children}
+          {sidebar}
+          <Footer />
+        </div>
+      </ThemeContextProvider>
+    </AuthContext.Provider>
+    // <div>
+    //   <ProgressBar />
+    //   <Header />
+    //   <div className={styles.layout}>{sidebar}</div>
+    //   {children}
+    //   <Footer />
+    // </div>
   );
 };
