f083b59c98204503e874a0200ace8fd12e1aad79
lesia <chlesya@yandex.ru>
2024-11-26 03:15:39 +0300

    ul style

--- src/compons/reviews/reviews.jsx
+++ src/compons/reviews/reviews.jsx
@@ -1,10 +1,11 @@
 import { Review } from "../review/review";
 import { Text } from "../text/text";
+import styles from "../ul/ul.module.css";
 export const Reviews = ({ reviews }) => {
   return (
     <div>
       <Text type={3}>Reviews</Text>
-      <ul style={{ color: "red", background: "lightblue" }}>
+      <ul className={styles.ul}>
         {reviews?.map(({ text, id }) => (
           <li key={id}>
             <Review text={text} />
