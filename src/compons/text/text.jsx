085aacd2a3a4120d959ceca02a680e2281d483eb
lesia <chlesya@yandex.ru>
2024-12-02 14:25:16 +0300

    tag

--- src/compons/text/text.jsx
+++ src/compons/text/text.jsx
@@ -1,28 +1,19 @@
 import classNames from "classnames";
 import styles from "./text.module.css";
-import React from "react";
+
 const Tags = {
   h1: "h1",
   h2: "h2",
   h3: "h3",
 };
-export function Text({ type, children }) {
-  if (Tags[type]) {
-    return React.createElement(
-      Tags[type],
-      {
-        className: classNames(
-          "text",
-          Tags[type] === "h1"
-            ? styles.h1
-            : Tags[type] === "h2"
-            ? styles.h2
-            : styles.h3
-        ),
-      },
-      children
-    );
-  }
+export const Text = ({ type, children }) => {
+  const Tag = Tags[type] || "span";
+  const TagStyle =
+    Tags[type] === "h1"
+      ? styles.h1
+      : Tags[type] === "h2"
+      ? styles.h2
+      : styles.h3;
 
-  return <span className={styles.text}>{children}</span>;
-}
+  return <Tag className={classNames("text", TagStyle)}>{children}</Tag>;
+};
